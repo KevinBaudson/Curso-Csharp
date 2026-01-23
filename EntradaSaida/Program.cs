cbe113412087da92b68c575fa6283d0c99d77093
KevinBaudson <kevinbaudson@hotmail.com>
2026-01-23 13:54:41 -0300

    Exercicio de lógica com mudanças de valores em variáveis. Invertendo nomes.

---
+++ EntradaSaida/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Kevin");
+            //Console.Write("Baudson");
+
+           //int codigo =  Console.Read();
+
+            //Console.WriteLine(codigo);
+
+            string mensagem = Console.ReadLine();
+            Console.WriteLine(mensagem);
+
+            Console.ReadKey();
+        }
+    }
+}
