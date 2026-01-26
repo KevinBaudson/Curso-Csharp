bf8bbe72f58a7bc817a0a396140f5edd97ac10cd
KevinBaudson <kevinbaudson@hotmail.com>
2026-01-26 10:48:39 -0300

    Entendendo sobre classes (mesmo arquivo e arquivos separados) .NET Framework

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaClasse mClasse = new MinhaClasse();
+            MinhaClasse m2 = null;
+            
+            OutraClasse outra = new OutraClasse();
+
+        }
+    }
+
+    class MinhaClasse
+    {
+
+    }
+}
